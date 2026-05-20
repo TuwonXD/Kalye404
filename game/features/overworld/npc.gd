e9042884a8c15197822b3dfc69c3a3e3e8869145
Samuel Antonio F. Oracion <samuelantonio.oracion@cvsu.edu.ph>
2026-05-20 22:13:22 +0800

    Update player textures and add new sprite assets
    - Renamed and updated multiple AtlasTexture resources in player.tscn for improved sprite management.
    - Added new sprite assets: "idle with slipper.png" and "throwing.png" along with their respective import configurations.
    - Adjusted player collision shape transform for better alignment.

--- game/features/overworld/npc.gd
+++ game/features/overworld/npc.gd
@@ -14,10 +14,6 @@ func _ready():
 	start_position = global_position
 	_pick_new_target()
 
-func _physics_process(delta):
-	if is_waiting:
-		return
-
 	if nav_agent.is_navigation_finished():
 		_start_waiting()
 		return
